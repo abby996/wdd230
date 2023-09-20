4da0060910389dc1ec3c2a2922452e6861acaacb
Abdias <abdiasbrevil@gmail.com>
2023-09-20 11:32:23 -0400

    update

---
+++ week01/scripts/getDates.js
@@ -0,0 +1,27 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Set the current year in the "currentYear" span
+document.getElementById('currentYear').textContent = currentYear;
+
+// Function to format the last modified date
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'numeric' ,day:'numeric' , hour:'numeric',minute:'numeric', second:'numeric'};
+    return date.toLocaleDateString(undefined, options);
+}
+
+// Get the last modified date and set it in the "lastModified" span
+let oLastModif = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = formatDate(oLastModif);
+
+
+
+// Navigation menu
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
