29980e077d104855e0327f5ebc63be7625cdd4c6
Abdias <abdiasbrevil@gmail.com>
2023-09-22 23:58:28 -0400

    team homepage

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -19,9 +19,31 @@ document.getElementById('lastModified').textContent = formatDate(oLastModif);
 // Navigation menu
 
 const hamButton = document.querySelector('#menu');
+
 const navigation = document.querySelector('.navigation');
+const main = document.querySelector("main");
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
-});
+
+    
+        if (modeButton.textContent.includes("☰")) {
+            main.style.background = "#000";
+            main.style.color = "#fff";
+            modeButton.textContent = "❎";
+        } else {
+            main.style.background = "#eee";
+            main.style.color = "#000";
+            modeButton.textContent = "☰";
+        }
+    });
+;
+
+
+
+
+
+
+
+
