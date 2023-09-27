ef3ace5c64fdb31907c0f71ae3301e10bd8e022c
Abdias <abdiasbrevil@gmail.com>
2023-09-26 21:29:29 -0400

    update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -13,3 +13,37 @@ function formatDate(date) {
 // Get the last modified date and set it in the "lastModified" span
 let oLastModif = new Date(document.lastModified);
 document.getElementById('lastModified').textContent = formatDate(oLastModif);
+
+
+
+// Navigation menu
+
+const hamButton = document.querySelector('#menu');
+
+const navigation = document.querySelector('.navigation');
+const main = document.querySelector("main");
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+    
+        if (modeButton.textContent.includes("☰")) {
+            main.style.background = "#000";
+            main.style.color = "#fff";
+            modeButton.textContent = "❎";
+        } else {
+            main.style.background = "#eee";
+            main.style.color = "#000";
+            modeButton.textContent = "☰";
+        }
+    });
+;
+
+
+
+
+
+
+
+
