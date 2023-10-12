e91f9dfdf033fa802495a4383294ecc694c2c42f
Abdias <abdiasbrevil@gmail.com>
2023-10-11 23:20:42 -0400

    updates

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -86,6 +86,35 @@ localStorage.setItem("numVisits-ls", numVisits);
 
 
 
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data); // Call the displayResults function with the data
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  apiFetch();
+
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  }
+  
 
 
