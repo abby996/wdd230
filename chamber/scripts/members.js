c3bc8ba72bec7471f092d95cc31fd4693abd0609
Abdias <abdiasbrevil@gmail.com>
2023-10-14 19:53:20 -0400

    new members

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,50 +1,18 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const membersContainer = document.getElementById('membersContainer');
-    const toggleButton = document.getElementById('toggleButton');
-    let gridView = true; // Initially, set to grid view
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
 
-    toggleButton.addEventListener('click', () => {
-        gridView = !gridView;
-        renderMembers();
-    });
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-    function renderMembers() {
-        const data = window.data; // Access data from data.json
-
-        if (gridView) {
-            membersContainer.innerHTML = ''; // Clear the container
-            data.companies.forEach((company) => {
-                const memberCard = document.createElement('div');
-                memberCard.classList.add('member-card');
-                memberCard.innerHTML = `
-                    <img src="${company.image}" alt="${company.name}">
-                    <h2>${company.name}</h2>
-                    <p>${company.address}</p>
-                    <p>${company.phone}</p>
-                    <a href="${company.website}" target="_blank">Website</a>
-                    <p>Membership Level: ${company.membership_level}</p>
-                    <p>${company.other_info}</p>
-                `;
-                membersContainer.appendChild(memberCard);
-            });
-        } else {
-            membersContainer.innerHTML = ''; // Clear the container
-            data.companies.forEach((company) => {
-                const memberItem = document.createElement('div');
-                memberItem.classList.add('member-item');
-                memberItem.innerHTML = `
-                    <h2>${company.name}</h2>
-                    <p>${company.address}</p>
-                    <p>${company.phone}</p>
-                    <a href="${company.website}" target="_blank">Website</a>
-                    <p>Membership Level: ${company.membership_level}</p>
-                    <p>${company.other_info}</p>
-                `;
-                membersContainer.appendChild(memberItem);
-            });
-        }
-    }
-
-    // Initial rendering
-    renderMembers();
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
 });
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
