ab064757ebff4b3187245f4e2c805b1ef04d1792
Abdias <abdiasbrevil@gmail.com>
2023-10-25 17:45:26 -0400

    update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,36 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Set the current year in the "currentYear" span
+document.getElementById('currentYear').textContent = currentYear;
+
+// Function to format the last modified date
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'numeric' ,day:'numeric' , hour:'numeric',minute:'numeric', second:'numeric'};
+    return date.toLocaleDateString(undefined, options);
+}
+
+// Get the last modified date and set it in the "lastModified" span
+let oLastModif = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = formatDate(oLastModif);
+
+//temperature
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    afficherTemperature();
+    
+});
+
+
+
+
+
+
+
+
+
+
 // Navigation menu
 
 const hamButton = document.querySelector('#menu');
@@ -29,34 +62,6 @@ hamButton.addEventListener('click', () => {
 
 
 
-// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
-const msToDays = 84600000;
-// today's date
-const theDateToday = new Date();
-
-// initialize display elements
-const todayElement = document.querySelector("#today");
-const christmasElement = document.querySelector("#christmas");
-const christmasDateElement = document.querySelector("#christmasDate");
-const daysElement = document.querySelector("#daysleft");
-
-// processing
-const today = Date.now();
-const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
-// check if is the waing days of December, if so ... change to next year.
-if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
-	christmasDate.setFullYear(christmasDate.getFullYear() + 1);
-}
-// find difference between epoch times in ms and convert to days
-let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
-
-todayElement.textContent = today;
-christmasElement.textContent = christmasDate.getTime();
-christmasDateElement.textContent = christmasDate;
-daysElement.textContent = `${daysleft.toFixed(0)} days`;
-
-
-
 //visitor
 
 // 1️⃣ Initialize display element variable
@@ -77,7 +82,6 @@ numVisits++;
 
 // 5️⃣ store the new visit total into localStorage, key=numVisits-ls
 localStorage.setItem("numVisits-ls", numVisits);
-
 
 
 
